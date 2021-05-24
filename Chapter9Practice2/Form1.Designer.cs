24a508213b51c7698adfaadd5c157265b052f102
昱辰 陳 <smalltaro8759387@gmail.com>
2021-05-24 16:27:42 +0800

    2021/05/24

--- Chapter9Practice2/Form1.Designer.cs
+++ Chapter9Practice2/Form1.Designer.cs
@@ -29,13 +29,135 @@ namespace Chapter9Practice2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1MSG = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageAdd = new System.Windows.Forms.TabPage();
+            this.textBoxFee = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.tabPageDelete = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPageAdd.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1MSG
+            // 
+            this.label1MSG.AutoSize = true;
+            this.label1MSG.Location = new System.Drawing.Point(473, 328);
+            this.label1MSG.Name = "label1MSG";
+            this.label1MSG.Size = new System.Drawing.Size(42, 15);
+            this.label1MSG.TabIndex = 0;
+            this.label1MSG.Text = "label1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageAdd);
+            this.tabControl1.Controls.Add(this.tabPageDelete);
+            this.tabControl1.Location = new System.Drawing.Point(146, 117);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(369, 187);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPageAdd
+            // 
+            this.tabPageAdd.Controls.Add(this.textBoxFee);
+            this.tabPageAdd.Controls.Add(this.textBoxName);
+            this.tabPageAdd.Controls.Add(this.label2);
+            this.tabPageAdd.Controls.Add(this.label1);
+            this.tabPageAdd.Controls.Add(this.buttonInsert);
+            this.tabPageAdd.Location = new System.Drawing.Point(4, 24);
+            this.tabPageAdd.Name = "tabPageAdd";
+            this.tabPageAdd.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAdd.Size = new System.Drawing.Size(361, 159);
+            this.tabPageAdd.TabIndex = 0;
+            this.tabPageAdd.Text = "Add";
+            this.tabPageAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBoxFee
+            // 
+            this.textBoxFee.Location = new System.Drawing.Point(82, 72);
+            this.textBoxFee.Name = "textBoxFee";
+            this.textBoxFee.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFee.TabIndex = 4;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(82, 27);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 23);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "年費";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "名稱";
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(56, 114);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(75, 23);
+            this.buttonInsert.TabIndex = 0;
+            this.buttonInsert.Text = "Add";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // tabPageDelete
+            // 
+            this.tabPageDelete.Location = new System.Drawing.Point(4, 24);
+            this.tabPageDelete.Name = "tabPageDelete";
+            this.tabPageDelete.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageDelete.Size = new System.Drawing.Size(361, 159);
+            this.tabPageDelete.TabIndex = 1;
+            this.tabPageDelete.Text = "Delete";
+            this.tabPageDelete.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label1MSG);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageAdd.ResumeLayout(false);
+            this.tabPageAdd.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1MSG;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageAdd;
+        private System.Windows.Forms.TextBox textBoxFee;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.TabPage tabPageDelete;
     }
 }
 
--- Chapter9Practice2/Form1.cs
+++ Chapter9Practice2/Form1.cs
@@ -7,15 +7,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Chapter9Practice2
 {
     public partial class Form1 : Form
     {
+        SqlConnection linkToDB;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefreshEverything();
+        }
+
+        private void RefreshEverything()
+        {
+
+            linkToDB = OpenConnection();
+
+            if (linkToDB == null) return;
+
+            label1MSG.Text = "資料庫連接成功";
+
+        }
+
+        private SqlConnection OpenConnection()
+        {
+            try
+            {
+                string connectionString = GetConnectionString();
+                SqlConnection myLocalLink = new SqlConnection(connectionString);
+                myLocalLink.Open();
+                return myLocalLink;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("資料庫連接失敗: " + ex.Message);
+                return null;
+            }
+
+        }
+
+        private string GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = @"(local)\SQLExpress";
+            builder.InitialCatalog = "StepSample";
+            builder.IntegratedSecurity = true;
+            return builder.ConnectionString;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            linkToDB.Close();
+            linkToDB.Dispose();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            string SQLCmd = "INSERT INTO Customer (FullName, AnnualFee) OUTPUT INSERTED.ID VALUES ('" +
+                textBoxName.Text.Trim() +
+                "', " +
+                textBoxFee.Text.Trim() + ")";
+
+            SqlCommand dataAction = new SqlCommand(SQLCmd, linkToDB);
+            try
+            {
+                // dataAction.ExecuteNonQuery();
+                // int newID = (int)dataAction,ExecuteScalar();
+                MessageBox.Show("成功新增ID=" + dataAction.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failure: " + ex.Message);
+            }
+
+        }
     }
 }
